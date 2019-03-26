2159574675cf463d4c83193a3fb228d1df978502
cosyphp <cosyphp@aliyun.com>
2019-03-26 12:23:31 +0800

    删除不用的store脚本文件

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import vueScrollBehavior from 'vue-scroll-behavior'
-import 'normalize.css'
-
-Vue.use(vueScrollBehavior, { router: router })
 
 /* eslint-disable no-new */
 new Vue({
